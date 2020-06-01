83d75e5a9d486fd97a2c463208840101c82e7f05
Siri A M <53007134+Siriayanur@users.noreply.github.com>
2020-06-01 17:46:59 +0530

    Add files via upload

---
+++ file1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+
+    char *name = "GitIsFun";
+    int count = 0;
+    for (int i = 0; name[i] != '\0'; i++)
+    {
+        count++;
+    }
+
+    cout << "String length is : " << count << endl;
+}
---
+++ file2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#define PI 3.1415
+
+int main()
+{
+
+    float radius = 4.5;
+
+    printf("The area of circle is %f", PI * radius * radius);
+}
